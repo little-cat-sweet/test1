8dc7b03d0dac5e1bb225f712b01f1d8463a9134f
Sky_Blue <2116400472@qq.com>
2023-02-09 22:57:05 +0800

    路由初次配置

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HelloComponent} from "./hello/hello.component";
+import {TitleComponent} from "./title/title.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'hello', component: HelloComponent},
+  {path:'title', component: TitleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
